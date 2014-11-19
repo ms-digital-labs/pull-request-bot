b9f154d1c25e632ff1ee5f5f06ca3366bd7e449d
Tom Lea <commit@tomlea.co.uk>
2014-11-19 10:43:13 +0000

    PEP8 that shit.

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ app = Flask(__name__)
 OAUTH_TOKEN = environ["OAUTH_TOKEN"]
 AUTH_HEADER = "token {}".format(OAUTH_TOKEN)
 
+
 def _template_path_for(name):
     return "templates/{}.md".format(name)
+
 
 def template_for(name):
     """
@@ -27,6 +29,7 @@ def template_for(name):
             return template_file.read()
     else:
         raise LookupError("Could not find the specified template")
+
 
 def post_comments_to(url, body):
     """
@@ -41,18 +44,21 @@ def post_comments_to(url, body):
     }
     return requests.post(url, headers=headers, data=json.dumps(data))
 
+
 @app.route("/pull_request", methods=['POST'])
 def pull_request():
     event_type = request.headers["X-GitHub-Event"]
     action = request.json["action"]
+    template_name = request.args.get("comment_template", "default")
 
     if action == "opened" and event_type == "pull_request":
         comments_url = request.json["pull_request"]["comments_url"]
-        template = template_for(request.args.get("comment_template", "default"))
+        template = template_for(template_name)
         post_comments_to(comments_url, template)
         return "ok"
     else:
         return "Thanks, but I don't care"
+
 
 if __name__ == "__main__":
     app.run(port=environ.get("PORT"), host='0.0.0.0')
